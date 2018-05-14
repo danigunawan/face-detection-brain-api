1e6dea54db3eb53235ed7757d67f512edc774bcd
virginia974 <39102527+virginia974@users.noreply.github.com>
2018-05-14 20:33:27 +0100

    Added files

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'Virginia',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send(database.users);})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
+
+
+
+
+
